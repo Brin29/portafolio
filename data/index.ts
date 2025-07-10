f6b7408fd6e924b269a2988413c1b94ce9637a61
Brin29 <breinerstivenparracortes50@gmail.com>
2025-07-09 23:12:49 -0500

    first commit

---
+++ data/index.ts
@@ -0,0 +1,140 @@
+export const navItems = [
+  {
+    name: "Sobre Mi",
+    link: "#sobre_mi",
+  },
+  {
+    name: "Experiencia",
+    link: "experiencia",
+  },
+  {
+    name: "Proyectos",
+    link: "proyectos",
+  },
+  {
+    name: "Habilidades",
+    link: "habilidades",
+  },
+];
+
+export const experience = [
+  {
+    date: "2025 - actualidad",
+    position: "Desarrollador Full stack",
+    description: {
+      details:
+        "En tecnoparque me encargue de liderar y desarrollar una gran variedad de proyectos para distintos clientes, me encarga de la comunicación y el desarrollo del proyecto",
+
+      projects: [
+        {
+          Contaflow:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        },
+        {
+          Agrobot:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        },
+        {
+          SiprotIA:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        },
+        {
+          Pets_Flyer:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+        },
+      ],
+    },
+    technologies: [
+      { name: "Djago", icon: "django" },
+      { name: "Vue JS", icon: "vue" },
+      { name: "N8n", icon: "n8n" },
+      { name: "MySQL", icon: "mySQL" },
+      { name: "Solana", icon: "solana" },
+      { name: "Python", icon: "python" },
+      { name: "FatApi", icon: "fastApi" },
+      { name: "Tailwind CSS", icon: "tailwind" },
+      { name: "Solana", icon: "solana" },
+      { name: "Tensorflow", icon: "tensorflow" },
+    ],
+  },
+  {
+    date: "2024 - 2025",
+    position: "Desarrollador Freelancer Full stack",
+    description: {
+      details:
+        "En mi paso como freelancer desarrolle y implemente dos proyectos para distintos clientes nacionales y internacionales",
+
+      projects: [
+        {
+          Rame: "Desarrolle e implemente una tienda virtual de venta de ropa, para un emprendimiento internacional",
+
+          Inventario:
+            "Desarrolle un inventario para un local de venta de productos de maquillaje",
+        },
+      ],
+    },
+    technologies: [
+      { name: "JAVA", icon: "java" },
+      { name: "Spring Boot", icon: "spring boot" },
+      { name: "React JS", icon: "react" },
+      { name: "Tailwind CSS", icon: "tailwind css" },
+      { name: "MySQL", icon: "mysql" },
+    ],
+  },
+];
+
+export const projects = [
+  {
+    title: "Generador de nombres con IA",
+    description:
+      "Desarrolle una aplicación web, integrandola con n8n, donde por medio de inteligencia artificial se generan nombres para mascotas, dependiendo unos datos que se ingresen",
+    image: "#",
+    technologies: [
+      { name: "Python", icons: "python" },
+      { name: "Django", icon: "django" },
+      { name: "Vue.js", icon: "vue" },
+      { name: "Tailwind CSS", icon: "tailwind" },
+      { name: "N8n", icon: "n8n" },
+    ],
+  },
+  {
+    title: "Acortador de Contraseñas",
+    description:
+      "Hice un acortador de contraseña usando Spring Boot, Docker y Redis, integrandola con la API de Bitly's, use redis para reducir y mejorar la velocidad del llamado a la API",
+    image: "#",
+    technologies: [
+      { name: "Spring Boot", icons: "spring boot" },
+      { name: "JAVA", icon: "java" },
+      { name: "Angular", icon: "angular" },
+      { name: "Tailwind CSS", icon: "tailwind" },
+      { name: "Docker", icon: "docker" },
+      { name: "Redis", icon: "redis" },
+    ],
+  },
+  {
+    title: "Chat con WebSocket",
+    description:
+      "Desarrolle una aplicacion implementando web socket, para un manejo de mensajes en tiempo real",
+    image: "#",
+    technologies: [
+      { name: "Spring Boot", icons: "spring boot" },
+      { name: "JAVA", icon: "java" },
+      { name: "Angular", icon: "angular" },
+      { name: "Tailwind CSS", icon: "tailwind" },
+    ],
+  },
+];
+
+export const technologies = [
+  { name: "Spring Boot", icons: "spring boot" },
+  { name: "JAVA", icon: "java" },
+  { name: "React JS", icon: "react" },
+  { name: "Next JS", icon: "next" },
+  { name: "Tailwind CSS", icon: "tailwind css" },
+  { name: "MySQL", icon: "mysql" },
+  { name: "Docker", icon: "docker" },
+  { name: "Redis", icon: "redis" },
+  { name: "Djago", icon: "django" },
+  { name: "FatApi", icon: "fastApi" },
+  { name: "Vue JS", icon: "vue" },
+];
