bc07566b855e092e4e5e20bb73d246675f53122f
Brin29 <breinerstivenparracortes50@gmail.com>
2025-07-10 22:02:38 -0500

    cambios

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Hero } from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="bg-gradient-to-t from-[#1e1f29] to-[#27187E] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 gap-2">
-
+    <main className="relative bg-gradient-to-t from-[#1e1f29] to-[#27187E] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 gap-2">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
     </main>
   );
 }
