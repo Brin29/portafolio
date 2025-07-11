f10e8fcc013d2cf2cbc58afe40a73d0d439b2d6f
Brin29 <breinerstivenparracortes50@gmail.com>
2025-07-11 17:04:06 -0500

    c

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,9 @@
-import { Spotlight } from "./ui/Spotlight"
 
 export const Hero = () => {
   return (
     <div className="h-[100vh]">
       <div>
-        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
       </div>
     </div>
-  )
-}
+  );
+};
