4a7e8d91927b0996273bbc6b182ea69952c061f7
Brin29 <breinerstivenparracortes50@gmail.com>
2026-04-14 12:37:31 -0500

    fix bugs

--- components/ui/three-d-card-demo.tsx
+++ components/ui/three-d-card-demo.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export function ThreeDCardDemo() {
   return (
 <CardContainer className="inter-var">
-  <CardBody className="flex items-center justify-center rounded-full w-[210px] h-[210px] md:h-[310px] md:w-[310px] bg-gradient-to-br from-blue-400 via-blue-400 to-blue-400">
+  <CardBody className="flex items-center justify-center rounded-full w-[310px] h-[310px]  bg-gradient-to-br from-blue-400 via-blue-400 to-blue-400">
     <CardItem translateZ="100" className="w-full flex items-center justify-center">
       <Image
         src="/photo.webp"
