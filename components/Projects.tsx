0ba3c8651bdb88dc94461dcc4cbca92145e49746
Brin29 <breinerstivenparracortes50@gmail.com>
2026-07-06 15:34:55 -0500

    ajusts

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,7 +17,7 @@ export const Projects = () => {
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            className="mb-30 md:mb-20 lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
           >
             <PinContainer title={title} href={link}>
               <div className="gap-3 flex items-center flex-col justify-center sm:w-96 w-[80vw] overflow-hidden mb-10">
